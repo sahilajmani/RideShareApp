1d1434d70dd07182cbb9056c8d98b51f29174bc1
sahil ajmani <sahil.ajmani1991@gmail.com>
2015-07-13 23:55:53 +0530

    added UserMapping for person matching algo

---
+++ src/pojos/UserMapping.java
@@ -0,0 +1,26 @@
+package pojos;
+
+public class UserMapping {
+private User userA;
+private User userB;
+private float distance;
+public User getUserA() {
+	return userA;
+}
+public void setUserA(User userA) {
+	this.userA = userA;
+}
+public User getUserB() {
+	return userB;
+}
+public void setUserB(User userB) {
+	this.userB = userB;
+}
+public float getDistance() {
+	return distance;
+}
+public void setDistance(float distance) {
+	this.distance = distance;
+}
+
+}
