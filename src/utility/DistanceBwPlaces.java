eab2e1b688e3c720add4b1774db9dc49ac29afc3
gargrish <rishabhgarg@nsitonline.in>
2015-07-14 22:22:28 +0530

    DIstanceBwPlaces.java committed

---
+++ src/utility/DistanceBwPlaces.java
@@ -0,0 +1,98 @@
+package utility;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.text.NumberFormat;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+
+public class DistanceBwPlaces {
+
+	  
+	  
+			public static double getDistanceandDuration(double sourceLat,
+					double sourceLon, double destLat, double destLon)
+					throws MalformedURLException, IOException, ProtocolException {
+				String originParams = sourceLat + "%20" + sourceLon;
+				String destinationParams = destLat + "%20" + destLon;
+		
+				 URL url = new URL(
+						"https://maps.googleapis.com/maps/api/distancematrix/json?origins="+ originParams +"&destinations="+destinationParams);
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				String line=null, outputString = "";
+
+			String json=null;
+			try{	BufferedReader reader = new BufferedReader(new InputStreamReader(
+						conn.getInputStream()));
+                   
+            //  BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream,"utf-8"),8);
+              StringBuilder sbuild = new StringBuilder();
+              //String line = null;
+              while ((line = reader.readLine()) != null) {
+                  sbuild.append(line);
+              }
+           //   inputStream.close();
+              json = sbuild.toString();               
+          } catch(Exception e) {
+          }
+
+
+          //now parse
+          JSONParser parser = new JSONParser();
+          Object obj=null;
+		try {
+			obj = parser.parse(json);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+          JSONObject jb = (JSONObject) obj;
+
+          //now read
+          JSONArray jsonObject1 = (JSONArray) jb.get("rows");
+          JSONObject jb1 = (JSONObject) jsonObject1.get(0);
+          JSONArray jsonObject2 = (JSONArray) jb1.get("elements");
+          JSONObject jb2 = (JSONObject) jsonObject2.get(0);
+          JSONObject jb3 = (JSONObject) jb2.get("distance");
+           outputString = (String) jb3.get("text");
+          String[] str;
+  		boolean isDistanceKm=true;
+  		if(outputString.contains("km"))
+  		{
+  		 str = outputString.split("km");
+  		}else
+  		{
+  	    str = outputString.split("m");
+  	    isDistanceKm=false;
+  		}
+  		String distance="";
+  		distance = str[0].trim();
+  	//	System.out.println(distance);
+  		NumberFormat format = NumberFormat.getInstance();
+  		Number number=null;
+		try {
+			number = format.parse(distance);
+			distance=number.toString();
+		} catch (java.text.ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+  //		System.out.println(number.toString());
+
+			
+			if(isDistanceKm)
+				 return((Double.parseDouble(distance)));
+			else
+				return((Double.parseDouble(distance))/1000);	
+			}
+}
