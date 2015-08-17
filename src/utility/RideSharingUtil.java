e66bcd58a46a1d40329ebd041e988fd73491a173
gargrish <rishabhgarg@nsitonline.in>
2015-08-18 01:49:42 +0530

    made dao thread safe

--- src/utility/RideSharingUtil.java
+++ src/utility/RideSharingUtil.java
@@ -10,37 +10,40 @@ import dao.DaoImpl;
 import dao.IChat;
 import dao.IChatImpl;
 
-
 public class RideSharingUtil {
 	private static DaoI dao = null;
 	private static SessionFactory sessionFactory = null;
 	public static IChat chatDAO = null;
 	public static Object mutex = new Object();
+	public static Object mutexDao = new Object();	
 
 	public static DaoI getDaoInstance() {
-		if (dao == null) {
-			dao = new DaoImpl();
+		synchronized (mutexDao) {
+			if (dao == null) {
+				dao = new DaoImpl();
+			}
 		}
 		return dao;
 	}
-	public static IChat getChatInstance(){
-		
-			synchronized (mutex) {
-				if (chatDAO == null) {	
+
+	public static IChat getChatInstance() {
+
+		synchronized (mutex) {
+			if (chatDAO == null) {
 				chatDAO = new IChatImpl();
-				}
 			}
+		}
 		return chatDAO;
 	}
-	
 
 	public static SessionFactory getSessionFactoryInstance() {
-			
+
 		if (sessionFactory == null) {
-			Configuration configuration = new Configuration().configure(GlobalConstants.HIBERNATE_CONFIG_XML);
-			ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
-		            configuration.getProperties()).build();
-		    sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+			Configuration configuration = new Configuration()
+					.configure(GlobalConstants.HIBERNATE_CONFIG_XML);
+			ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+					.applySettings(configuration.getProperties()).build();
+			sessionFactory = configuration.buildSessionFactory(serviceRegistry);
 		}
 		return sessionFactory;
 	}
