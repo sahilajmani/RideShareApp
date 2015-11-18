578e9bc7b6e937593141c7ce77f4ba10359cc25d
sahil ajmani <sahil.ajmani1991@gmail.com>
2015-11-18 23:29:21 +0530

    forgot to checkin

--- src/Test/AddMatches.java
+++ src/Test/AddMatches.java
@@ -7,7 +7,7 @@ public class AddMatches {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		DaoImpl db=new DaoImpl();
-		db.matchTest("user5");
+//		db.matchTest("user5");
 	}
 
 }
